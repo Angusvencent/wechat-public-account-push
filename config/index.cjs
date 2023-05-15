a872299a2dc6cbf4348fa8dedcc0ab25027f8cfd
Angusvencent <115913884+Angusvencent@users.noreply.github.com>
2023-05-15 22:31:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRo3M6Xu8EMrhFVhiOYe1ywVsjbA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'suawZGa9O17aKVPbZ1HjFglykPTWnNdMKGX-EvqL7UQ',
+      useTemplateId: 'xTrqHdNeudSTEQ7AKExQfJKrCT4CylDxFS3kntXWpj0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-31',
       festivals: [
